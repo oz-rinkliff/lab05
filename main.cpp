0e6e14a4c6ac45a9ecc02dbb7d9a6e92681357cb
oz-rinkliff <125987842+oz-rinkliff@users.noreply.github.com>
2023-02-21 16:05:54 -0500

    main push

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-//AHHHHHHHH MAIN_FILE_FIXME AHHHHHHHHHH
+// My Name: FIXME_NAME
+// My Class: CPSC 1021
+// Date: 02.22.2023
+// Desc: FIXME_DESCRIPTION
+// Time: FIXME_TIME
+
+// included libraries and header files 
+#include <iostream>
+//include additional libraries here
+
+#include "Car.h"
+#include "Electric.h"
+#include "Employee.h"
+#include "Hybrid.h"
+#include "Invoice.h"
+#include "Motorcycle.h"
+#include "Student.h"
+#include "Utility.h"
+#include "Vendor.h"
+#include "Visitor.h"
+using namespace std;
+
+int main() {
+    // varible declarations
+
+    // prompt
+
+    return 0;
+}
