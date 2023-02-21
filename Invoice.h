548e353037f36d7ccdc589df3f7dcd21c8f2ff30
oz-rinkliff <125987842+oz-rinkliff@users.noreply.github.com>
2023-02-21 09:31:05 -0500

    push

---
+++ Invoice.h
@@ -0,0 +1,12 @@
+// Invoice.h is the class specification file
+#ifndef INVOICE_H
+#define INVOICE_H
+
+// Invoice class declaration
+class Invoice {
+    private:
+    //PRIVATE_FIXME
+    public:
+    //PUBLIC_FIXME
+};
+#endif
